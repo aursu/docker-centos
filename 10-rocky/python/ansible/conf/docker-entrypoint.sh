5109fe8995e23f9e0b355b85adc87a53e4c4c86b
Oleksandr Ursu <alexander.ursu@gmail.com>
2026-02-02 03:46:56 +0100

    Refactor Ansible environment activation in entrypoint scripts

--- 10-rocky/python/ansible/conf/docker-entrypoint.sh
+++ 10-rocky/python/ansible/conf/docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -f ~/.bashrc ]; then
-    . ~/.bashrc
-fi
-
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 source /var/ansible/ansible-env/bin/activate
+unset VIRTUAL_ENV_DISABLE_PROMPT
+
+if [ "$(id -un)" = "ansible" ] && command -v gpgconf >/dev/null; then
+    gpgconf --launch gpg-agent
+fi
 
 exec "$@"
