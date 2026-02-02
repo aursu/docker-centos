a1d4ccc7474dbf22af48fd4db6d9fabdebe2a4aa
Oleksandr Ursu <alexander.ursu@gmail.com>
2026-02-02 01:16:40 +0100

    Restrict gpgconf launch to 'ansible' user in entrypoint script

--- 10-rocky/python/ansible/conf/docker-entrypoint.sh
+++ 10-rocky/python/ansible/conf/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 source /var/ansible/ansible-env/bin/activate
 
-if command -v gpgconf >/dev/null; then
+if [ "$(id -un)" = "ansible" ] && command -v gpgconf >/dev/null; then
     gpgconf --launch gpg-agent
 fi
 
