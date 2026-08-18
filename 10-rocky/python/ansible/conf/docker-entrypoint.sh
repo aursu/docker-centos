0c172b94c4af93104bd4640c5c73505a019a016b
Oleksandr Ursu <alexander.ursu@gmail.com>
2026-08-18 17:22:38 +0200

    feat: remove virtualenv and streamline Python installation in Dockerfiles for Rocky Linux

--- 10-rocky/python/ansible/conf/docker-entrypoint.sh
+++ 10-rocky/python/ansible/conf/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-source ~/.venv/bin/activate
-unset VIRTUAL_ENV_DISABLE_PROMPT
-
 if [ "$(id -un)" = "ansible" ] && command -v gpgconf >/dev/null; then
     gpgconf --launch gpg-agent
 fi
